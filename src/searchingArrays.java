e4735cb61b1c85269372872fdbb047ef91c8f9b5
stephenOwino <36981339s@gmail.com>
2025-12-14 03:30:49 +0300

    added binary search algorithm

--- src/searchingArrays.java
+++ src/searchingArrays.java
@@ -1,2 +1,14 @@
+import java.util.Arrays;
+
 public class searchingArrays {
+
+    static void main() {
+        String[] sArray = {"Stephen" , "Cairo" , "Joyce" , "Betty" , "Gabriel"};
+        Arrays.sort(sArray);
+        System.out.println(Arrays.toString(sArray));
+        if (Arrays.binarySearch(sArray, "Betty") >=0 ){
+            System.out.println("Found Betty In The List!");
+
+        }
+    }
 }
