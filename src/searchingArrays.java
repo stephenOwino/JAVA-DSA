601a120cd34824e9520eb5b3518d3e6077a62a8f
stephenOwino <36981339s@gmail.com>
2025-12-14 03:48:36 +0300

    added arrays.equals method for comparing arrays

--- src/searchingArrays.java
+++ src/searchingArrays.java
@@ -3,12 +3,22 @@ import java.util.Arrays;
 public class searchingArrays {
 
     static void main() {
-        String[] sArray = {"Stephen" , "Cairo" , "Joyce" , "Betty" , "Gabriel"};
+        String[] sArray = {"Stephen", "Cairo", "Joyce", "Betty", "Gabriel"};
         Arrays.sort(sArray);
         System.out.println(Arrays.toString(sArray));
-        if (Arrays.binarySearch(sArray, "Betty") >=0 ){
+        if (Arrays.binarySearch(sArray, "Betty") >= 0) {
             System.out.println("Found Betty In The List!");
 
         }
+        //comparing if arrays are equal!
+        int[] s1 = {1, 2, 3, 4, 5, 6};
+        int[] s2 = {1, 2, 3, 4, 5, 6};
+
+        if (Arrays.equals(s1, s2)) {
+            System.out.println("Both arrays are equal!");
+        }else{
+                System.out.println("The above arrays are not equal!");
+            }
+        }
     }
-}
+
